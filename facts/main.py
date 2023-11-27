997559a583bc3be5ee672534495cd83bd6bb8cc7
sunkay <sunkay11@gmail.com>
2023-11-27 08:00:43 -0500

    using embeddings

--- facts/main.py
+++ facts/main.py
@@ -1,3 +1,24 @@
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 from dotenv import load_dotenv
 
 load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=200,
+    chunk_overlap=0
+)
+
+loader = TextLoader("facts.txt")
+docs = loader.load_and_split(
+    text_splitter=text_splitter
+)
+
+for doc in docs:
+    print(doc.page_content)
+    print("\n")
+
